911aa976209ff5dd5d4e8514635055a509478551
detalhe <updudu@hotmail.com>
2024-05-27 16:22:53 -0300

    added dashboard and fixed saidas-eventos anchor

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,27 @@
+const Database = require('../utils/database');
+
 class HomeController {
-    constructor() {
-    }
+    constructor() {}
 
     async homeView(req, res) {
         try {
-            res.render('home');
+            const db = new Database();
+
+            const usuariosCount = await db.ExecutaComando('SELECT COUNT(*) as count FROM usuarios');
+            const animaisCount = await db.ExecutaComando('SELECT COUNT(*) as count FROM animais');
+            const empresasParceirasCount = await db.ExecutaComando('SELECT COUNT(*) as count FROM empresas_parceiras');
+            const doadoresCount = await db.ExecutaComando('SELECT COUNT(*) as count FROM doadores');
+            const produtosCount = await db.ExecutaComando('SELECT COUNT(*) as count FROM produtos');
+            const saidasEventosCount = await db.ExecutaComando('SELECT COUNT(*) as count FROM saidas_eventos');
+
+            res.render('home', {
+                usuariosCount: usuariosCount[0].count,
+                animaisCount: animaisCount[0].count,
+                empresasParceirasCount: empresasParceirasCount[0].count,
+                doadoresCount: doadoresCount[0].count,
+                produtosCount: produtosCount[0].count,
+                saidasEventosCount: saidasEventosCount[0].count
+            });
         } catch (error) {
             res.status(500).send("Erro ao carregar página inicial: " + error.message);
         }
